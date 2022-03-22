278becd7dae8eb2ecb8d57c7571ad9e3c0745ac8
gmodrz <78708240+gmodrz@users.noreply.github.com>
2022-03-22 13:49:34 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace _2020_ZALICZENIE
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridDoctors = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSpecialization = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDoctors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridDoctors
+            // 
+            this.gridDoctors.AllowUserToAddRows = false;
+            this.gridDoctors.AllowUserToDeleteRows = false;
+            this.gridDoctors.BackgroundColor = System.Drawing.Color.White;
+            this.gridDoctors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDoctors.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
+            this.gridDoctors.Location = new System.Drawing.Point(271, 10);
+            this.gridDoctors.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridDoctors.Name = "gridDoctors";
+            this.gridDoctors.RowHeadersWidth = 51;
+            this.gridDoctors.RowTemplate.Height = 24;
+            this.gridDoctors.Size = new System.Drawing.Size(510, 346);
+            this.gridDoctors.TabIndex = 0;
+            this.gridDoctors.DoubleClick += new System.EventHandler(this.gridDoctors_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Specjalizacja";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Miasto";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(93, 20);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(174, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSpecialization
+            // 
+            this.txtSpecialization.Location = new System.Drawing.Point(93, 43);
+            this.txtSpecialization.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSpecialization.Name = "txtSpecialization";
+            this.txtSpecialization.Size = new System.Drawing.Size(174, 20);
+            this.txtSpecialization.TabIndex = 5;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(93, 66);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(174, 20);
+            this.txtCity.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Imie i Nazwisko";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(176, 99);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 36);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Dodaj Lekarza";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(88, 99);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 36);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(9, 193);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(90, 36);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Załaduj liste";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(106, 149);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 167);
+            this.panel1.TabIndex = 12;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 366);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtSpecialization);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gridDoctors);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form";
+            this.Text = "Lekarze";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDoctors)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridDoctors;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSpecialization;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace _2020_ZALICZENIE
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+
+        }
+        Doktor model = new Doktor();
+
+        void Clear()
+        {
+            txtCity.Text = txtName.Text = txtSpecialization.Text = "";
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            model.City = txtCity.Text.Trim();
+            model.Name = txtName.Text.Trim();
+            model.Specialization = txtSpecialization.Text.Trim();
+            using (HospitalDBEntities db = new HospitalDBEntities())
+            {
+                if (string.IsNullOrWhiteSpace(txtCity.Text) || string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtSpecialization.Text))
+                {
+                    MessageBox.Show("Pola nie mogą być puste!");
+                }
+                else
+                {
+                    if (model.Id == 0)
+                    {
+                        db.Doktors.Add(model);
+                    }
+                    else
+                    {
+                        db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                    Clear();
+                    MessageBox.Show("Działanie pomyślne!");
+                    gridDoctors.DataSource = db.Doktors.ToList();
+                    btnDelete.Enabled = false;
+                    btnAdd.Text = "Dodaj Lekarza";
+                }
+            }
+
+        }
+
+        
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            HospitalDBEntities db = new HospitalDBEntities();
+            gridDoctors.DataSource = db.Doktors.ToList();
+            this.gridDoctors.Columns[0].Visible = false;
+        }
+
+        private void gridDoctors_DoubleClick(object sender, EventArgs e)
+        {
+            if (gridDoctors.CurrentRow.Index != -1)
+            {
+                model.Id = Convert.ToInt32(gridDoctors.CurrentRow.Cells["Id"].Value);
+                using (HospitalDBEntities db = new HospitalDBEntities())
+                {
+                    model = db.Doktors.Where(x => x.Id == model.Id).FirstOrDefault();
+                    txtCity.Text = model.City;
+                    txtName.Text = model.Name;
+                    txtSpecialization.Text = model.Specialization;
+                }
+            }
+            btnAdd.Text = "Aktualizuj";
+            btnDelete.Enabled = true;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Jesteś pewien że chcesz usunąć lekarza z listy?","Usuwanko",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (HospitalDBEntities db = new HospitalDBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == System.Data.Entity.EntityState.Detached)
+                        db.Doktors.Attach(model);
+                    db.Doktors.Remove(model);
+                    db.SaveChanges();
+                    Clear();
+                    MessageBox.Show("Usunięto pomyślnie!");
+                    gridDoctors.DataSource = db.Doktors.ToList();
+                    btnAdd.Text = "Dodaj Lekarza";
+                    btnDelete.Enabled = false;
+                }
+            }
+        }
+
+        public delegate int numery(int n1, int n2);
+        public int numerki(int a, int b)
+        {
+            return a + b;
+        }
+        private void Form_Load(object sender, EventArgs e)
+        { 
+            numery n = new numery(numerki);
+            int wynik = n(2, 3);
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Red, 3);
+            Rectangle a = new Rectangle(20, 20, 20, 20);
+            Graphics g = e.Graphics;
+            g.DrawEllipse(p, a);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
